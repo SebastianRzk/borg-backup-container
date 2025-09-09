f49f678fedbc06e6d156c042845ae0ad7885d58e
SebastianRzk <github@sebastianruziczka.de>
2025-09-09 12:42:02 +0200

    add arm64 and basic updates

--- borg-backup-docker/entrypoint.sh
+++ borg-backup-docker/entrypoint.sh
@@ -8,11 +8,11 @@ declare -p | grep -Ev 'BASHOPTS|BASH_VERSINFO|EUID|PPID|SHELLOPTS|UID' > /contai
 # Setup a cron schedule
 echo "SHELL=/bin/bash
 BASH_ENV=/container.env
-$BORG_BACKUP_CRON python3 /main.py
+$BORG_BACKUP_CRON source /venv/bin/activate && python3 /main.py
 # This extra line makes it a valid cron" > scheduler.txt;
 
+source /venv/bin/activate
 python3 /main.py --initial-checkup
 
 crontab scheduler.txt;
 crond -f;
-
